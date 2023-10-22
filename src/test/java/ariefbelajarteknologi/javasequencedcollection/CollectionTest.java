e961146c98ee38923bb5b0b4cb9713b5001a58f0
StandHereHermawan <nolifegansaya@gmail.com>
2023-10-22 20:42:28 +0700

    Sequenced Collection, modify unit test

--- src/test/java/ariefbelajarteknologi/javasequencedcollection/CollectionTest.java
+++ src/test/java/ariefbelajarteknologi/javasequencedcollection/CollectionTest.java
@@ -1,4 +1,22 @@
 package ariefbelajarteknologi.javasequencedcollection;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SequencedCollection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CollectionTest {
+
+    @Test
+    void collection() {
+        SequencedCollection<String> listHilmi = new ArrayList<>();
+        listHilmi.add("Hilmi");          // Hilmi
+        listHilmi.add("Akbar");          // Hilmi, Akbar
+        listHilmi.add("Muharrom");       // Hilmi, Akbar, Muharrom
+
+        assertEquals(List.of("Hilmi", "Akbar", "Muharrom"), listHilmi);
+    }
 }
